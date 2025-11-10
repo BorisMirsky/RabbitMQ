ea14005de6b3e737c2e191d8cc46ccdef0d3974e
Boris Mirsky <boris.y.mirsky@gmail.com>
2025-11-10 12:53:23 +0300

    dotnet in process

--- dotnet/consumerConsoleApp/consumerConsoleApp/Program.cs
+++ dotnet/consumerConsoleApp/consumerConsoleApp/Program.cs
@@ -1,33 +1,46 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
+using System.Diagnostics;
 using System.Text;
 
-static async void Main(string[] args)
+
+namespace ConsumeConsoleApp
 {
-
-    string host = "amqps://jmnvgeqr:1FNeqMtZKcTKJSMqPRDVQUj9Oo1iBB-r@chimpanzee.rmq.cloudamqp.com:5671/jmnvgeqr";
-    var factory = new ConnectionFactory() {Uri = new Uri(host) };
-    using var connection = await factory.CreateConnectionAsync();
-    using var channel = await connection.CreateChannelAsync();
-    await channel.QueueDeclareAsync(queue: "dotnetRabbit",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
-
-    var consumer = new EventingBasicConsumer(channel);
-    consumer.Received += (model, ea) =>
+    public static class RabbitConsumer
+    {
+        static async void RunConsum()
         {
-            var body = ea.Body.ToArray();
-            var message = Encoding.UTF8.GetString(body);
-            Console.WriteLine(" [x] Received {0}", message);
-        };
-    channel.BasicConsumeAsync(queue: "MyQueue",
-                             autoAck: true,
-                             consumer: consumer);
+            string host = "amqps://jmnvgeqr:1FNeqMtZKcTKJSMqPRDVQUj9Oo1iBB-r@chimpanzee.rmq.cloudamqp.com:5671/jmnvgeqr";
+            var factory = new ConnectionFactory() { Uri = new Uri(host) };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+            using (channel.QueueDeclareAsync(queue: "dotnetRabbit",
+                                             durable: false,
+                                             exclusive: false,
+                                             autoDelete: false,
+                                             arguments: null))
+            {
+                var consumer = new AsyncEventingBasicConsumer(channel);
+                consumer.ReceivedAsync += async (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Debug.WriteLine(" [x] Received {0}", message);
+                    };
+                string consumerMessage = await channel.BasicConsumeAsync(queue: "dotnetRabbit",
+                                         autoAck: true,
+                                         consumer: consumer);
+                Debug.WriteLine("consumerMessage");
+                Debug.WriteLine(consumerMessage);
+            }
 
-    Console.WriteLine(" Press [enter] to exit.");
-    Console.ReadLine();
+
+            static void Main(string[] args)
+            {
+                RunConsum();
+            }
+
+        }
     }
 }
